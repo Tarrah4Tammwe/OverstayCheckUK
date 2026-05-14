bbb444f0b3dfd0814142c0330f7fb2ae735072c2
Tarrah4Tammwe <133949570+Tarrah4Tammwe@users.noreply.github.com>
2026-05-14 14:09:16 +0100

    Fix: next.config.js redirects to fix www redirect + HTTPS enforcement

--- next.config.js
+++ next.config.js
@@ -9,13 +9,34 @@ const nextConfig = {
             key: 'X-DNS-Prefetch-Control',
             value: 'on',
           },
+          {
+            key: 'X-Robots-Tag',
+            value: 'index, follow'
+          },
+          {
+            key: 'X-Frame-Options',
+            value: 'SAMEORIGIN'
+          },
         ],
       },
     ]
   },
   redirects: async () => {
     return [
-      // Redirect non-www to www for overstaycheck.co.uk
+      // Force HTTPS
+      {
+        source: '/:path*',
+        destination: 'https://www.overstaycheck.co.uk/:path*',
+        permanent: true,
+        has: [
+          {
+            type: 'header',
+            key: 'x-forwarded-proto',
+            value: 'http'
+          },
+        ],
+      },
+      // Redirect non-www to www (optional, if canonical is www)
       {
         source: '/:path*',
         destination: 'https://www.overstaycheck.co.uk/:path*',
